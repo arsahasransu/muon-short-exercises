d985caa81aa049f0225f6be9bf538685a6731f7b
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2020-09-18 13:02:28 +0200

    Z peak fit fixed

--- CMSSW_10_6_8/src/MuonExercises/MuonExercises2/FitInvMass.C
+++ CMSSW_10_6_8/src/MuonExercises/MuonExercises2/FitInvMass.C
@@ -8,17 +8,27 @@ void FitInvMass() {
   auto RecM = (TH1F*)file->Get("demo/h_RecDiMuonM");
   auto GenM = (TH1F*)file->Get("demo/h_GenDiMuonM");
 
-  auto recFit = new TF1("Reco_Fit","[0]*TMath::Voigt(x, [1], [2])", 86., 96.);
-  recFit->SetParameter(0, 100.);
+  auto recFit = new TF1("Reco_Fit","[0]*TMath::Voigt(x-[1], [2], [3])", 75., 105.);
   recFit->SetParameter(1, 91.);
   recFit->SetParameter(2, 3.);
   recFit->SetParameter(3, 3.);
-  RecM->Fit(recFit,"","", 86., 96.);
-  auto genFit = new TF1("Genp_Fit","[0]*TMath::BreitWigner(x, [1], [2])", 86., 96.);
-  genFit->SetParameter(1, 91.);
+  RecM->Fit(recFit,"","", 75., 105.);
+  /*
+  auto c1 = new TCanvas();
+  RecM->Draw();
+  recFit->Draw("SAME");
+  */
+  
+  auto genFit = new TF1("Genp_Fit","[0]*TMath::BreitWigner(x, [1], [2])", 75., 105.);
+  genFit->SetParameter(1, 90.);
   genFit->SetParameter(2, 3.);
-  GenM->Fit(genFit,"","", 86., 96.);
-
+  GenM->Fit(genFit,"","", 75., 105.);
+  /*
+  auto c1 = new TCanvas();
+  GenM->Draw();
+  genFit->Draw("SAME");
+  */
+  
   std::cout<<RecM->Integral()<<std::endl;
   std::cout<<GenM->Integral()<<std::endl;
 }
